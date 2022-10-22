6fa7035f00e3044856d76e5d44d073136a6080c8
Sergio De Luca <sdlbsso@gmail.com>
2022-10-22 17:13:33 -0300

    rutas hijas heroes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { AuthModule } from './auth/auth.module';
 
 const routes : Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule)
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule)
+  },
   {
     path: '404',
     component: ErrorPageComponent
