fa9bdf7deae0a8dc2e77843df5fc7a52ef61c635
Sergio De Luca <sdlbsso@gmail.com>
2022-10-27 16:06:55 -0300

    listado de heroes

---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Heroe } from '../interface/heroes.interface';
+
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+
+  constructor(private http: HttpClient) { }
+
+  getHeroes(): Observable<Heroe[]>{
+    return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+  }
+}
